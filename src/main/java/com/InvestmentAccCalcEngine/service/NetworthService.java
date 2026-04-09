f9ac1b542c1cfeb8987bf656ab978c0aea994f91
bergs692 <bergs692@umn.edu>
2026-04-09 12:57:21 -0500

    init

--- src/main/java/com/InvestmentAccCalcEngine/service/NetworthService.java
+++ src/main/java/com/InvestmentAccCalcEngine/service/NetworthService.java
@@ -19,5 +19,23 @@ public class NetworthService {
     private final MortgageService mortgageService;
     private final RentalPropertyService rentalPropertyService;
 
+    public NetworthService(SalaryService salaryService, BankAccountService bankAccountService,
+                             MortgageService mortgageService, RentalPropertyService rentalPropertyService) {
+        this.bankAccountService = bankAccountService;
+        this.mortgageService = mortgageService;
+        this.rentalPropertyService = rentalPropertyService;
+    }
+
+    public BigDecimal calculateNetworth(String accountNumber){
+        BigDecimal networth =  bankAccountService.getBalance(accountNumber);
+
+        BigDecimal mortgageValue = BigDecimal.ZERO;
+        for (ActiveMortgage mortgage : mortgageService.getActiveMortgages()) {
+            if (!mortgage.isFullyPaid()) {
+                monthlyMortgageCost = monthlyMortgageCost.add(mortgage.getTotalMonthlyCharge());
+            }
+        }
+    }
+
 
 }
